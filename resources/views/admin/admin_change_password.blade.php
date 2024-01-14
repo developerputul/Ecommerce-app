0228343f94bde5c64794be13b2e63efaabec59c5
developerputul <putulsentongchangya7@gmail.com>
2024-01-14 23:22:33 +0600

    database updated

--- resources/views/admin/admin_change_password.blade.php
+++ resources/views/admin/admin_change_password.blade.php
@@ -6,7 +6,7 @@
 <div class="container-fluid">
 
     <div class="row">
-        <div class="col-12">
+        <div class="col-10">
             <div class="card">
                 <div class="card-body">
     <!-----------------------------------changed password and update------------------------------->
